ddbd87ae1d9fc8897b489165ae52ac3610941c88
Felix Cheysson <felix@cheysson.fr>
2018-10-05 18:28:53 +0200

    Added function taking a Hawkes model in argument.

--- R/mle.R
+++ R/mle.R
@@ -31,3 +31,31 @@ mle <- function(data, T, init=c(1,1,2), opts = NULL, ...) {
   
   return( model )
 }
+
+#' Title
+#'
+#' @param model 
+#' @param opts 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+mle_ <- function(model, opts = NULL, ...) {
+  # Likelihood function (for nloptr)
+  nlopt_fn <- function(param_) {
+    model$param <- param_
+    return( lapply(X = model$likngrad(), FUN = "-") )
+  }
+  
+  if (is.null(opts))
+    opts <- list("algorithm" = "NLOPT_LD_LBFGS") else
+      if (is.null(opts[["algorithm"]]))
+        opts <- c(opts, "algorithm" = "NLOPT_LD_LBFGS")
+  
+  opt <- nloptr(x0 = model$param, eval_f = nlopt_fn, lb = rep(.0001, 3), opts = opts, ...)
+  
+  model$vcov <- solve(-model$hessian())
+  model$opt <- opt
+}
